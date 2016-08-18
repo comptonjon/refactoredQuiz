4e5bd32be03c1d1f23f5e4648306de5bb129215e
Jon Compton <yar12345@hotmail.com>
2016-08-18 14:08:49 -0700

    Removed redundant functions

---
+++ QuizApp/GameModel.swift
@@ -0,0 +1,58 @@
+//
+//  GameModel.swift
+//  TrueFalseStarter
+//
+//  Created by Jonathan Compton on 8/13/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import GameKit
+
+struct GameModel {
+    let questionArray:[Question]
+    let numberOfQuestions = 6
+    var questionsLeft:Int
+    let questions = QuestionModel()
+    var usedNumber:[Int] = []
+    var correctQuestions:Int
+    init(){
+        questionArray = questions.getQuestionArray()
+        questionsLeft = numberOfQuestions
+        correctQuestions = 0
+    }
+    mutating func getQuestion()->Question{
+        let randomNumber = getRandomNumber()
+        return questionArray[randomNumber]
+        
+        
+    }
+    
+    mutating func getRandomNumber()->Int{
+        var randomNumber = 0
+        var needNumber = true
+        while needNumber{
+            randomNumber = GKRandomSource.sharedRandom().nextIntWithUpperBound(questionArray.count)
+            if !usedNumber.contains(randomNumber){
+                usedNumber.append(randomNumber)
+                questionsLeft -= 1
+                needNumber = false
+            }
+        }
+        
+        return randomNumber
+    }
+    
+    mutating func pointScored(){
+        correctQuestions += 1
+    }
+    
+    mutating func reset(){
+        questionsLeft = questionArray.count
+        usedNumber = []
+        correctQuestions = 0
+    }
+    
+    func getScore()->Int{
+        return correctQuestions
+    }
+}
---
+++ QuizApp/QuestionModel.swift
@@ -0,0 +1,38 @@
+//
+//  QuestionModel.swift
+//  TrueFalseStarter
+//
+//  Created by Jonathan Compton on 8/13/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import GameKit
+
+struct QuestionModel {
+    var questions:[Question] = []
+    
+    init (){
+        loadArray()
+    }
+
+    mutating func loadArray(){
+        
+        questions.append(Question(question: "This was the only US President to serve more than two consecutive terms.", answers: ["George Washington", "Franklin D. Roosevelt", "Woodrow Wilson", "Andrew Jackson"], answerIndex: 1))
+        questions.append(Question(question: "Which of the following countries has the most residents?", answers: ["Nigeria", "Russia", "Iran", "Vietnam"], answerIndex: 0))
+        questions.append(Question(question: "In what year was the United Nations founded?", answers: ["1918", "1919", "1945", "1954"], answerIndex: 2))
+        questions.append(Question(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", answers: ["Paris", "Washington D.C.", "New York City", "Boston"], answerIndex: 2))
+        questions.append(Question(question: "Which nation produces the most oil?", answers: ["Iran", "Iraq", "Brazil", "Canada"], answerIndex: 3))
+        questions.append(Question(question: "Which country has most recently won consecutive World Cups in Soccer?", answers: ["Italy", "Brazil", "Argentina", "Spain"], answerIndex: 1))
+        questions.append(Question(question: "Which of the following rivers is longest?", answers: ["Yangtze", "Mississippi", "Congo", "Mekong"], answerIndex: 1))
+        questions.append(Question(question: "Which city is oldest?", answers: ["Mexico City", "Cape Town", "San Juan", "Sydney"], answerIndex: 0))
+        questions.append(Question(question: "Which country was the first to allow women to vote in national elections?", answers: ["Poland", "United States", "Sweden", "Senegal"], answerIndex: 0))
+        questions.append(Question(question: "Which of these countries won the most medals in the 2012 Summer Games?", answers: ["France", "Germany", "Japan", "Great Britain"], answerIndex: 3))
+        
+    }
+    
+
+    func getQuestionArray()->[Question]{
+        return questions
+    }
+}
+
